2595e8f3ee3bfbfd83f8bd7fd17ac46903ba035e
Jonathan <jonitoadriano@gmail.com>
2022-12-08 17:12:59 -0600

    added update code

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,6 +1,20 @@
 class RecipeController < ApplicationController
   def index
-    render json: {message: "test"}
+    recipe = Cook.all
+    render json: recipe.as_json
+  end
+  def show
+    recipe = Cook.find_by(id: params[:id])
+    render json: recipe.as_json
+  end
+  def update
+    recipe = Cook.find_by(id: params[:id])
+    recipe.name = params[:name]
+    recipe.ingredients = params[:ingredients]
+    recipe.prep_time = params[:prep_time]
+    recipe.instructions = params[:instructions]
+    recipe.save
+    render json: recipe.as_json
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
  get "/recipes" =>"recipe#index"
+ get "/recipes/:id" =>"recipe#show"
+ patch "/recipes/:id" =>"recipe#update"
 end
