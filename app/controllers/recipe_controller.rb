d508b8a94a79f595c567632b5ebf9df658baeb91
Jonathan <jonitoadriano@gmail.com>
2022-12-08 17:25:12 -0600

    created create REST

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -21,5 +21,14 @@ class RecipeController < ApplicationController
     recipe.delete
     render json: {message: "Recipe deleted"}
   end
+  def create
+    recipe = Cook.create(
+      name: params[:name],
+      ingredients: params[:ingredients],
+      prep_time: params[:prep_time],
+      instructions: params[:instructions]
+    )
+    render json: recipe.as_json
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
  get "/recipes/:id" =>"recipe#show"
  patch "/recipes/:id" =>"recipe#update"
  delete "/recipes/:id" =>"recipe#destroy"
+ post "/recipes" =>"recipe#create"
 end
